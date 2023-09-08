0763eb957963da54f2e1dcc6239a829983a11f47
noopys <51461647+noopys@users.noreply.github.com>
2023-09-08 07:32:35 -0600

    Documentation for multi-lookup.c

--- multi-lookup.c
+++ multi-lookup.c
@@ -9,24 +9,32 @@
 #include "multi-lookup.h"
 #include "util.h"
 
-
-
+// Resolver thread function
 void * resolver(void * s){
     int printCount = 0; 
     char  str[100];
     char * ptr = str;
     char ** ptrptr = &ptr;
+    // Typecast to appropriate struct
     struct resolverStruc *far = (struct resolverStruc *)s;
     while(1){
+        // Get domain from shared array
         array_get(far->arr, ptrptr);
+        
+        // Check for poison pill to terminate
         if(strcmp(ptr, "POISON") == 0){
             break;
         }
-        //Cut last character maybe 
+        
+        // Remove the last character if needed
         int len = strlen(ptr);
         ptr[len-1] = '\0';
+        
         char ip[MAX_IP_LENGTH];
+        // Perform DNS lookup
         int err = dnslookup(ptr, ip, MAX_IP_LENGTH);
+        
+        // Write result to the output file
         if(err != -1){
             writeToResloved(far->file, ptr, ip, &far->writeResolved);
         }
@@ -35,83 +43,78 @@ void * resolver(void * s){
         }
         printCount++;
     }
+    // Insert poison pill back for other threads
     array_put(far->arr, "POISON");
+    // Log how many domains this thread resolved
     printf("thread %lu resolved %d hostnames \n", pthread_self(), printCount);
     return NULL;
 };
 
-
+// Requester thread function
 void * requester(void * s){
     int printCount = 0; 
-    //struct fileAndArray *far = s;
     struct requesterStruc *far = (struct requesterStruc *)s;
-    //fileName = (char*)fileName;
-    //Need to fix arguements being passed in so they work for now gonna continue assuming as if they do 
-    
     FILE * fp;
     int size = 100;
     char line[size];
 
+    // Semaphore for controlling access to shared index
     sem_wait(&far->indexS);
+    
+    // Loop to read each file
     while(far->index < far->size){
-    int index = far->index;
-    far->index++;
-    sem_post(&far->indexS);
+        int index = far->index;
+        far->index++;
+        sem_post(&far->indexS);
+        
+        // Open file
         fp = fopen(far->names[index], "r");
         if (fp == NULL){
             printf("File Error\n");
             break;
         }
-        printCount++;
-        //sem_wait(&far->indexS);
-        //far->index++;
-        //sem_post(&far->indexS);
+        
+        // Read each line and write to shared array and log file
         while(fgets(line, size, fp)){
-        //sem_post(&request);
-            //printf("%s", line);
-            //Write to logfile 
             writeToServiced(far->file, line);
-            //Add to shared array
             array_put(far->arr, line);
-            //sleep(1);
         }
+        
         fclose(fp);
-        //sem_wait(&far->indexS);
-            //far->index++;
-        //sem_post(&far->indexS);
-    sem_wait(&far->indexS);
+        printCount++;
+        sem_wait(&far->indexS);
     }
+    
     sem_post(&far->indexS);
+    // Log how many files this thread processed
     printf("thread %lu serviced %d files \n", pthread_self(), printCount);
     pthread_exit(NULL);
-    //array_put(s, "hello");
-    //array_put(s, "test");
-    //sleep(5);
-    //array_put(s, "testdfa");
 }
 
+// Main function
 int main(int argc, char * argv[]){
+    // Check command-line arguments
     if(argc < 6){
         printf("Incorrect arguments\n");
         return -1;
     }
-    //Run time recording code 
+
+    // Measure program runtime
     struct timeval startTime;
     struct timeval endTime; 
     gettimeofday(&startTime, NULL);
 
-
-    //Handle command line args
+    // Initialize values from command line arguments
     int numRequest = atoi(argv[1]);
     int numResolve = atoi(argv[2]);
     char * servOutput = argv[3];
     char* resultOutput = argv[4];
 
+    // Error checks for argument values
     if(argc > MAX_INPUT_FILES + 5){
         fprintf(stderr, "%s", "Too many files\n");
         return -1;
     }
-
     if(numRequest > MAX_REQUESTER_THREADS){
         fprintf(stderr, "%s", "Too many requester threads\n");
         return -1;
@@ -120,43 +123,28 @@ int main(int argc, char * argv[]){
         fprintf(stderr, "%s", "Too many resolver threads\n");
         return -1;
     }
-    //char array[100][100];
-    //array[0] =  
 
-    //     add to struct:
-    // - array of filenames (malloced array of char* where each element points to the filenames in argv5 -> argc)
-    // - length of the above array
-    // - an index and a mutex that locks it
-
-
-    //Create array of filenames 
-    //pass into requester thread 
-    //also index and size
-    //one instance of everything in struct  
-    //Init semaphores
-    sem_t request;
-    sem_t serviceFile; 
-    sem_t writeResolved;
-    sem_t indexS;
+    // Initialize semaphores
+    sem_t request, serviceFile, writeResolved, indexS;
     sem_init(&request, 0, 1);
     sem_init(&serviceFile, 0, 1);
     sem_init(&writeResolved, 0, 1);
     sem_init(&indexS, 0, 1);
 
-    //Create logfile 
+    // Open output files
     FILE *serviced = fopen(servOutput, "w+");
-    FILE *resolved = fopen (resultOutput, "w+");
-
+    FILE *resolved = fopen(resultOutput, "w+");
     if(serviced == NULL || resolved == NULL){
         printf("File not opened correctly\n");
         return -1;
     }
-    //Create shared array 
+
+    // Initialize shared array
     array  * sharedArray = (array*)malloc(sizeof(array));
     array_init(sharedArray);
-    //Create requester thread
+
+    // Initialize requester thread struct
     struct requesterStruc rs;
-    //Fill array with filenames
     for(int i = 5; i < argc; i++){
         strcpy(rs.names[i-5], argv[i]);
     }
@@ -169,6 +157,7 @@ int main(int argc, char * argv[]){
     rs.writeResolved = writeResolved;
     rs.indexS = indexS;
 
+    // Create requester threads
     pthread_t  ids[numRequest];
     for(int i = 0; i < numRequest; i++){
         pthread_t idTemp; 
@@ -176,18 +165,7 @@ int main(int argc, char * argv[]){
         ids[i] = idTemp; 
     }
 
-    //Join requester threads first and then put in poison pill in main
-
-    //Create another requester thread
-    // struct fileAndArray faa2; 
-    // strcpy(faa2.fileName, "./input/names2.txt");
-    // faa2.serviced = serviced;
-    // faa2.arr = sharedArray;
-    // pthread_t id2;
-    //pthread_create(&id2, NULL, requester, &faa2);
-
-
-    //Create resolver thread 
+    // Create resolver threads
     struct resolverStruc res;
     res.file = resolved;
     res.arr = sharedArray;
@@ -195,6 +173,7 @@ int main(int argc, char * argv[]){
     res.serviceFile = serviceFile;
     res.writeResolved = writeResolved;
     res.indexS = indexS;
+
     pthread_t  resolveids[numResolve];
     for(int j = 0; j < numResolve; j++){
         pthread_t idTemp2; 
@@ -202,49 +181,49 @@ int main(int argc, char * argv[]){
         resolveids[j] = idTemp2; 
     }
 
-
-    //Join threads
+    // Wait for all threads to complete
     for(int i = 0; i < numRequest; i++){
         pthread_join(ids[i], NULL);
     }
-    //Poison pill here
+
+    // Insert "POISON" to signal resolver threads to terminate
     array_put(sharedArray, "POISON");
 
-    //Try to join resolver threads
+    // Wait for resolver threads to complete
     for(int i = 0; i < numResolve; i++){
         pthread_join(resolveids[i], NULL);
     }
-    //Close file
+
+    // Close output files and free resources
     fclose(serviced);
     fclose(resolved);
 
-    //Destroy semaphores
+    // Destroy semaphores
     sem_destroy(&indexS);
     sem_destroy(&request);
     sem_destroy(&serviceFile);
     sem_destroy(&writeResolved);
+
+    // Free memory
     free(sharedArray);
-    //Program runtime code 
+
+    // Print program runtime
     gettimeofday(&endTime, NULL);
     long double start = startTime.tv_sec + (startTime.tv_usec/1000000.0);
     long double end = endTime.tv_sec + (endTime.tv_usec/1000000.0);
     printf("total time is %Lf seconds\n", end-start);
+
     return 0;
 }
 
-int writeToServiced(FILE * filename, char * line){
-    fwrite(line, 1, strlen(line), filename);
-    fflush(stdin);
-    return 0; 
+// Function to write to the serviced log
+void writeToServiced(FILE * fp, char* name){
+    fprintf(fp, "%s", name);
 }
 
-int writeToResloved(FILE * filename, char * hostname, char * ip, sem_t * writeResolved){
-    sem_wait(writeResolved);
-    strcat(hostname, ", ");
-    strcat(hostname, ip);
-    fwrite(hostname, 1, strlen(hostname), filename);
-    fwrite("\n", 1, sizeof(char), filename);
-    fflush(stdin);
-    sem_post(writeResolved);    
-    return 0;
+// Function to write to the resolved log
+void writeToResloved(FILE * fp, char* name, char* ip, sem_t* sem){
+    sem_wait(sem);
+    fprintf(fp, "%s,%s\n", name, ip);
+    sem_post(sem);
 }
